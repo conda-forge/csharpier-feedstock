005ddfa5aa519972853040179942024bc935c3e3
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-09 21:07:49 -0800

    update to 1.2.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,22 +12,22 @@ rm -rf .config/dotnet-tools.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
 dotnet publish --no-self-contained Src/CSharpier.Cli/CSharpier.Cli.csproj --output ${PREFIX}/libexec/${PKG_NAME} \
     --framework net${framework_version} -p:TreatWarningAsErrors=false
-rm ${PREFIX}/libexec/${PKG_NAME}/dotnet-csharpier
+rm ${PREFIX}/libexec/${PKG_NAME}/CSharpier
 
 # Create bash and batch wrappers
 tee ${PREFIX}/bin/dotnet-csharpier << EOF
 #!/bin/sh
-exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/csharpier/dotnet-csharpier.dll "\$@"
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/csharpier/Csharpier.dll "\$@"
 EOF
 chmod +x ${PREFIX}/bin/dotnet-csharpier
 
 tee ${PREFIX}/bin/dotnet-csharpier.cmd << EOF
-call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX\libexec\csharpier\dotnet-csharpier.dll %*
+call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX\libexec\csharpier\Csharpier.dll %*
 EOF
 
 # Download dependency licenses with dotnet-project-licenses
 tee ignored_packages.json << EOF
-["Ignore", "YamlDotNet"]
+["Ignore", "YamlDotNet", "StrongNamer"]
 EOF
 
 dotnet-project-licenses --input Src/CSharpier.Cli/CSharpier.Cli.csproj -t -d license-files -ignore ignored_packages.json
